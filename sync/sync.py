220209e9b05a67ec5fd3360ba52239e07b788517
vagrant <vagrant@django.vagrantup.com>
2016-03-07 08:32:30 +0000

    增加同步limit_value到Redis

--- sync/sync.py
+++ sync/sync.py
@@ -70,13 +70,13 @@ def sync_redis_app():
         st[app["app_id"]] = app
 
     json_value = json.dumps(st, ensure_ascii=False)
-    print json_value
+    # print json_value
     for config in RDS_HOST:
         try:
             rds = redis.Redis(host=config["host"], port=config["port"], db=0)
             rds.ping()
-            rds.hset("__gateway_redis__", "app", json_value)
-            rds.hset("__gateway_redis__", "api_update_time", int(time.time()))
+            rds.hset("__gateway_redis__", "shared_inner_app", json_value)
+            rds.hset("__gateway_redis__", "app_update_time", int(time.time()))
             rds.save()
         except Exception, msg:
             logging.error([('topic', 'sync_gw_redis_app'), ('key', "Redis"), ("msg", str(msg))])
@@ -86,8 +86,40 @@ def sync_redis_app():
     return True
 
 
+def sync_redis_limit():
+    if len(RDS_HOST) == 0:
+        return False
+
+    sql = "select b.app_id, a.url, b.limit_value from gateway_service a, gateway_request_limit b where a.id=b.service_id"
+    limits = _mysql_exec(sql)
+    if len(limits) == 0:
+        logging.error([('topic', 'sync_redis_limit'), ('key', "sync_redis_limit() failed"), ("sql", sql)])
+        return False
+
+    st = {}
+    for limit in limits:
+        st[limit["app_id"]] = {limit["url"]: limit["limit_value"]}
+
+    json_value = json.dumps(st, ensure_ascii=False)
+    # print json_value
+    for config in RDS_HOST:
+        try:
+            rds = redis.Redis(host=config["host"], port=config["port"], db=0)
+            rds.ping()
+            rds.hset("__gateway_redis__", "app_and_url_limit", json_value)
+            rds.hset("__gateway_redis__", "limit_update_time", int(time.time()))
+            rds.save()
+        except Exception, msg:
+            logging.error([('topic', 'sync_redis_limit'), ('key', "Redis"), ("msg", str(msg))])
+            return False
+    logging.info([('topic', 'sync_redis_limit'), ('key', "sync to redis ok[limit]")])
+
+    return True
+
+
 def main():
     sync_redis_app()
+    sync_redis_limit()
 
 
 if __name__ == '__main__':

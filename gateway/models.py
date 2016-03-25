d4d7a536b2e7f07df9289dc8b967dd60cc35b859
vagrant <vagrant@django.vagrantup.com>
2016-03-25 11:31:55 +0800

    增加update_time字段

--- gateway/models.py
+++ gateway/models.py
@@ -15,6 +15,7 @@ class App(models.Model):
     is_encrypt = models.CharField("is_encrypt", choices=CHOICES, default="0", max_length=2)
     remark1 = models.TextField("remark1", max_length=1024, default="")
     remark2 = models.TextField("remark2", max_length=1024, default="")
+    update_time = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = 'App'
@@ -25,10 +26,11 @@ class App(models.Model):
 
 
 class AppAdmin(admin.ModelAdmin):
-    list_display=('app_id', 'name', 'app_key', 'app_secret', 'is_encrypt')
+    list_display=('app_id', 'name', 'app_key', 'app_secret', 'is_encrypt', 'update_time')
     search_fields=('name',)    
     list_filter = ('is_encrypt',)                 
     ordering = ('app_id','name')
+    readonly_fields = ('update_time',)
     #fields = ('app_id', 'name', 'app_key', 'app_secret', 'is_encrypt', 'remark1', 'remake2')
 
     fieldsets = (
@@ -40,6 +42,7 @@ class Service(models.Model):
     name = models.CharField("name", max_length=16, default="")
     url = models.CharField("url", max_length=64, default="")
     text = models.TextField("text", max_length=128, default="")
+    update_time = models.DateTimeField(auto_now=True)
     class Meta:
         verbose_name = 'Service'
         verbose_name_plural = 'Service'
@@ -49,24 +52,27 @@ class Service(models.Model):
 
 
 class ServiceAdmin(admin.ModelAdmin):
-    list_display=('name', 'url', 'text')
+    list_display=('name', 'url', 'text', 'update_time')
     search_fields=('name', 'url')    
     ordering = ('name','url')
     fields = ('name', 'url', 'text')
+    readonly_fields = ('update_time',)
 
 
 class Request_limit(models.Model):
     app = models.ForeignKey('App', verbose_name="list of apps")
     service = models.ForeignKey('Service', verbose_name="list of services")
     limit_value = models.IntegerField(default=0)
+    update_time = models.DateTimeField(auto_now=True)
 
 class RequestlimitAdmin(admin.ModelAdmin):
     empty_value_display = '-empty-'
-    list_display=('app', 'service', 'limit_value')
+    list_display=('app', 'service', 'limit_value', 'update_time')
     search_fields=('app','service')    
     list_filter = ('limit_value',)                 
     ordering = ('app','service')
     fields = ('app', 'service', 'limit_value')
+    readonly_fields = ('update_time',)
 
 
 admin.site.register(App, AppAdmin)
